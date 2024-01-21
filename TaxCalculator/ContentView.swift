2f6d6c899d2550777d75fb92f841b1ea4cd1d4b7
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 12:38:26 +0900

    gitignore追加

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            TextField("ここに文字を入力", text: $inputText).keyboardType(.numberPad)
+            Text("価格: \(inputText)")
+            Text("消費税8%: \((Double(inputText) ?? 0) * 0.08)")
+            Text("消費税10%: \((Double(inputText) ?? 0) * 0.1)")
+            
         }
         .padding()
     }
