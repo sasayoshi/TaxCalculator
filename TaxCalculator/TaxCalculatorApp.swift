328985c23bcef63bb465eeecb7ca0dc7ff3253d8
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 12:01:33 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by sasayoshi on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
